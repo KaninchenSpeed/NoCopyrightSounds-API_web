5c62a6aa152a43eb6ec1f0c2eb98fcd8a1b32607
KaninchenSpeed <fhs222@outlook.de>
2022-06-07 13:17:16 +0000

    added client

---
+++ src/modules/Client.ts
@@ -0,0 +1,63 @@
+import SongCache from '../helpers/SongCache.js'
+import * as web from './web.js'
+
+import type Artist from '../api/Artist'
+import type Filter from '../helpers/search'
+
+export interface NCSClientOptions {
+  proxy_url: string
+  use_cache?: boolean
+  cache_path?: string
+  detailed_log?: boolean
+}
+
+export default class NCSClient {
+  private proxy_url: string
+  private detailed_log: boolean
+  protected cache: SongCache | undefined
+
+  public constructor(options: NCSClientOptions) {
+    this.proxy_url = options.proxy_url ?? ''
+    this.detailed_log = options.detailed_log ?? false
+
+    if (options.use_cache) {
+      this.cache = new SongCache({
+        proxy_url: this.proxy_url,
+        cache_path: options.cache_path,
+        detailed_log: this.detailed_log
+      })
+    }
+  }
+
+  public getCache(): SongCache | undefined {
+    return this.cache
+  }
+
+  public async getSongs(page = 1) {
+    if (this.cache) {
+      const start = Math.max((page - 1) * 20, 0)
+      return this.cache.songs.slice(
+        start,
+        Math.min(start + 20, this.cache.songs.length)
+      )
+    } else {
+      return await web.getSongs(this.proxy_url, page)
+    }
+  }
+
+  public async search(filter: Filter, page = 1) {
+    if (this.cache && this.detailed_log) {
+      console.warn(
+        'This method is not cached! Use an alterate search engine insted like https://www.npmjs.com/package/flexsearch'
+      )
+    }
+    return await web.search(this.proxy_url, filter, page)
+  }
+
+  public async getArtistInfo(artist_url: string | Artist) {
+    if (this.cache && this.detailed_log) {
+      console.warn('This method is not cached (coming soon)!')
+    }
+    return await web.getArtistInfo(this.proxy_url, artist_url)
+  }
+}
